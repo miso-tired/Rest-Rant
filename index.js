24c99703c68c12e9c5b6621f9aea7820916e5a4d
Anthony <anthonylsa12@gmail.com>
2024-02-03 11:58:27 -0800

    Added places controller

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+app.use('/places', require('./controllers/places'))
+
 app.get('/', (req, res) => {
     res.send('Hello World HAHA!')
 })
 
+
+// Wildcard/404 route
 app.get('*', (req, res) => {
     res.status('<h1>404 Page</h1>')
 })
