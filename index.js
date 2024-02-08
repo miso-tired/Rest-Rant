f25dd52aecb09c75e3f4a40d2e14876ade6ac6f1
Anthony <anthonylsa12@gmail.com>
2024-02-07 21:24:20 -0800

    finished /places page

--- index.js
+++ index.js
@@ -8,14 +8,20 @@ app.engine('jsx', require('express-react-views').createEngine())
 
 app.use('/places', require('./controllers/places'))
 
-app.get('/', (req, res) => {
+// Home Page
+app.get('/', (req, res) => { 
     res.render('home')
 })
 
+// Index Page
+app.get('/places', (req, res) => {
+    res.render('index')
+})
 
 // Wildcard/404 route
 app.get('*', (req, res) => {
     res.render('error404')
 })
+
 
 app.listen(process.env.PORT)
