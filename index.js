ad60a491b9e529a465c706837ec613d3b6d87097
Anthony <anthonylsa12@gmail.com>
2024-02-07 20:18:11 -0800

    Adding 404 page

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
 
 // Wildcard/404 route
 app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 Page</h1>')
+    res.render('error404')
 })
 
 app.listen(process.env.PORT)
---
+++ views/error404.jsx
@@ -0,0 +1,15 @@
+const React = require('react')
+const Def = require('./default')
+
+function error404() {
+    return (
+        <Def>
+            <main>
+                <h1>404: Page Not Found</h1>
+                <p>Sorry, we can't find the page you are looking for! D:</p>
+            </main>
+        </Def>
+    )
+}
+
+module.exports = error404
