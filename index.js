ab7bc57d2e1f05baab58c4312657093278d3e3a0
Anthony <anthonylsa12@gmail.com>
2024-02-08 21:44:47 -0800

    Added CSS to project

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
+// Controllers and Routes
 app.use('/places', require('./controllers/places'))
 
 // Home Page
