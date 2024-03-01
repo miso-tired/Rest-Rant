19e10965a211122ffce03530958f367cedc18772
Anthony <anthonylsa12@gmail.com>
2024-03-01 15:10:32 -0800

    Connected to MONGODB

--- controllers/places.js
+++ controllers/places.js
@@ -1,99 +1,139 @@
-const router = require('express').Router()
-const places = require('../models/placesarray.js')
-const express = require('express')
+// const router = require('express').Router()
+// const places = require('../models/placesarray.js')
+// const express = require('express')
 
-// GET /places/new
-router.get ('/new', (req, res) => {
-    res.render('places/new')
-})
+// // GET /places/new
+// router.get ('/new', (req, res) => {
+//     res.render('places/new')
+// })
 
-// POST /places
-router.post('/', (req, res) => {
-    if (!req.body.pic) {
-      req.body.pic = 'http://placekitten.com/400/400'
-    }
-    if (!req.body.city) {
-      req.body.city = 'Random City'
-    }
-    if (!req.body.state) {
-      req.body.state = 'USA'
-    }
-    places.push(req.body)
-    res.redirect('/places')
-  })
+// // POST /places
+// router.post('/', (req, res) => {
+//     if (!req.body.pic) {
+//       req.body.pic = 'http://placekitten.com/400/400'
+//     }
+//     if (!req.body.city) {
+//       req.body.city = 'Random City'
+//     }
+//     if (!req.body.state) {
+//       req.body.state = 'USA'
+//     }
+//     places.push(req.body)
+//     res.redirect('/places')
+//   })
   
-// GET /places
+// // GET /places
+// router.get('/', (req, res) => {
+//       res.render('places/index', { places })
+//   })
+
+
+// // Error 404 Page
+// router.get('/:id', (req, res) => {
+//     let id = Number(req.params.id)
+//     if (isNaN(id)) {
+//       res.render('error404')
+//     }
+//     else if (!places[id]) {
+//         res.render('error404')
+//     }
+//     else {
+//       res.render('places/show', { place: places[id], id })
+//     }
+// })
+
+
+// // PUT Route
+// router.put('/:id', (req, res) => {
+//   let id = Number(req.params.id)
+//   if (isNaN(id)) {
+//       res.render('error404')
+//   }
+//   else if (!places[id]) {
+//       res.render('error404')
+//   }
+//   else {
+//       if (!req.body.pic) {
+//         req.body.pic = 'http://placekitten.com/400/400'
+//       }
+//       if (!req.body.city) {
+//         req.body.city = 'Unkown'
+//       }
+//       if (!req.body.city) {
+//         req.body.state = 'USA'
+//       }
+//       places[id] = req.body
+//       res.redirect(`/places/${id}`)
+//   }
+// })
+
+// // EDIT Route
+// router.get('/:id/edit', (req, res) => {
+//   let id = Number(req.params.id)
+//   if (isNaN(id)) {
+//     res.render('error404')
+//   } 
+//   else if (!places[id]) {
+//     res.render('error404')
+//   }
+//   else {
+//     res.render('places/edit', { place: places[id] })
+//   }
+// })
+
+// // DELETE Route
+// router.delete('/:id', (req, res) => {
+//   let id = Number(req.params.id)
+//   if (isNaN(id)) {
+//     res.render('error404')
+//   }
+//   else if (!places[id]) {
+//     res.render('error404')
+//   }
+//   else {
+//     places.splice(id, 1)
+//     res.redirect('/places')
+//   }
+// })
+
+// module.exports = router
+
+const router = require('express').Router()
+
 router.get('/', (req, res) => {
-      res.render('places/index', { places })
-  })
+  res.send('GET /places stub')
+})
 
+router.post('/', (req, res) => {
+  res.send('POST /places stub')
+})
 
-// Error 404 Page
+router.get('/new', (req, res) => {
+  res.render('places/new')
+})
+
 router.get('/:id', (req, res) => {
-    let id = Number(req.params.id)
-    if (isNaN(id)) {
-      res.render('error404')
-    }
-    else if (!places[id]) {
-        res.render('error404')
-    }
-    else {
-      res.render('places/show', { place: places[id], id })
-    }
+  res.send('GET /places/:id stub')
 })
 
-
-// PUT Route
 router.put('/:id', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-      res.render('error404')
-  }
-  else if (!places[id]) {
-      res.render('error404')
-  }
-  else {
-      if (!req.body.pic) {
-        req.body.pic = 'http://placekitten.com/400/400'
-      }
-      if (!req.body.city) {
-        req.body.city = 'Unkown'
-      }
-      if (!req.body.city) {
-        req.body.state = 'USA'
-      }
-      places[id] = req.body
-      res.redirect(`/places/${id}`)
-  }
+  res.send('PUT /places/:id stub')
 })
 
-// EDIT Route
-router.get('/:id/edit', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-    res.render('error404')
-  } 
-  else if (!places[id]) {
-    res.render('error404')
-  }
-  else {
-    res.render('places/edit', { place: places[id] })
-  }
-})
-
-// DELETE Route
 router.delete('/:id', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-    res.render('error404')
-  }
-  else if (!places[id]) {
-    res.render('error404')
-  }
-  else {
-    places.splice(id, 1)
-    res.redirect('/places')
-  }
+  res.send('DELETE /places/:id stub')
+})
+
+router.get('/:id/edit', (req, res) => {
+  res.send('GET edit form stub')
+})
+
+router.post('/:id/rant', (req, res) => {
+  res.send('GET /places/:id/rant stub')
+})
+
+router.delete('/:id/rant/:rantId', (req, res) => {
+    res.send('GET /places/:id/rant/:rantId stub')
 })
 
 module.exports = router
--- models/modelsindex.js
+++ models/modelsindex.js
@@ -1,8 +1,17 @@
+// const mongoose = require('mongoose')
+
+// mongoose.connect(process.env.MONGO_URI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// })
+
+// module.exports.Place = require('./places')
+
 const mongoose = require('mongoose')
 
 mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+  useNewUrlParser: true, 
+  useUnifiedTopology: true
 })
 
-module.exports.Place = require('./places')
+module.exports.Place = require('./placesarray.js')
